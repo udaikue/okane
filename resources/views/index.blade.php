88f5ef4be58bcfecad52cb37f7e8a911a4c8a97b
udaikue <ikueikue@gmail.com>
2023-03-05 11:12:14 +0900

    Generated OutgoingController

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0,
+        maximum-scale=1.0, minimum-scale=1.0">
+    <title>outgoings</title>
+</head>
+<body>
+    <h1>シュッピ</h1>
+</body>
+</html>
