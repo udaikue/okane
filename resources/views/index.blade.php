847d040bc49cef7e1cad6ba37c267d70eb7948a7
udaikue <ikueikue@gmail.com>
2023-03-05 22:07:42 +0900

    Installed tailwindcss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,27 +5,28 @@
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0,
         maximum-scale=1.0, minimum-scale=1.0">
+        @vite('resources/css/app.css')
     <title>outgoings</title>
 </head>
 <body>
-    <h1>シュッピ</h1>
+    <div class="text-2xl px-3 py-3">2023年3月</div>
     <div>
-        <table>
+        <table class="table-auto mt-3 ml-3">
             <thead>
                 <tr>
-                    <th>カテゴリ</th>
-                    <th>円</th>
-                    <th>メモ</th>
-                    <th>繰り返し</th>
+                    <th class="border-b px-1 py-1">カテゴリ</th>
+                    <th class="border-b px-1 py-1">円</th>
+                    <th class="border-b px-1 py-1">メモ</th>
+                    <th class="border-b px-1 py-1">繰り返し</th>
                 </tr>
             </thead>
             <tbody>
             @foreach($outgoings as $outgoing)
                 <tr>
-                    <td>{{ $outgoing->category }}</td>
-                    <td>{{ $outgoing->yen }}</td>
-                    <td>{{ $outgoing->memo }}</td>
-                    <td>{{ $outgoing->repeated }}</td>
+                    <td class="border-b px-1 py-1">{{ $outgoing->category }}</td>
+                    <td class="border-b px-1 py-1">{{ $outgoing->yen }}</td>
+                    <td class="border-b px-1 py-1">{{ $outgoing->memo }}</td>
+                    <td class="border-b px-1 py-1">{{ $outgoing->repeated }}</td>
                 </tr>
             @endforeach
             </tbody>
