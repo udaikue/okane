34a07bc3799fefee2f7cfdee93443a6083f10efc
udaikue <ikueikue@gmail.com>
2023-03-25 22:18:55 +0900

    Added index page features

--- app/Models/Outgoing.php
+++ app/Models/Outgoing.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Outgoing extends Model
 {
     use HasFactory;
+
+    public function scopeIndex($query, $yearMonth)
+    {
+        return $query->where('date', 'LIKE', $yearMonth);
+    }
+    public function scopeCategorySum($query, $category)
+    {
+        return $query->where('category', $category)->sum('yen');
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,26 +9,78 @@
     <title>outgoings</title>
 </head>
 <body>
-    <div class="text-2xl px-3 py-3">2023年3月</div>
+    <div class="text-2xl mt-3 ml-3 px-10 py-2">{{ $viewYear }}年{{ $viewMonth }}月</div>
     <div>
         <table class="table-auto mt-3 ml-3">
-            <thead>
-                <tr>
-                    <th class="border-b px-1 py-1">カテゴリ</th>
-                    <th class="border-b px-1 py-1">円</th>
-                    <th class="border-b px-1 py-1">メモ</th>
-                    <th class="border-b px-1 py-1">繰り返し</th>
-                </tr>
-            </thead>
             <tbody>
-            @foreach($outgoings as $outgoing)
                 <tr>
-                    <td class="border-b px-1 py-1">{{ $outgoing->category }}</td>
-                    <td class="border-b px-1 py-1">{{ $outgoing->yen }}</td>
-                    <td class="border-b px-1 py-1">{{ $outgoing->memo }}</td>
-                    <td class="border-b px-1 py-1">{{ $outgoing->repeated }}</td>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::RENT }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::RENT] }}</td>
                 </tr>
-            @endforeach
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::INSURANCE }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::INSURANCE] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::UTILITY }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::UTILITY] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::FOOD }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::FOOD] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::COMMODITY }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::COMMODITY] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::COMMUNICATION }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::COMMUNICATION] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::MEDICAL }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::MEDICAL] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::BEAUTY }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::BEAUTY] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::AMUSEMENT }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::AMUSEMENT] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::CLOTHING }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::CLOTHING] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::GOODS }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::GOODS] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::SOCIAL }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::SOCIAL] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::BOOKS }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::BOOKS] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::EATING_OUT }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::EATING_OUT] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::TRIP }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::TRIP] }}</td>
+                </tr>
+                <tr>
+                    <td class="border-b px-10 py-2">{{ App\Consts\CategoryConst::SPECIAL }}</td>
+                    <td class="border-b px-10 py-2">{{ $categorySums[App\Consts\CategoryConst::SPECIAL] }}</td>
+                </tr>
+                <tr>
+                    <td class="px-10 py-2">合計</td>
+                    <td class="px-10 py-2">{{ array_sum($categorySums) }} 円</td>
+                </tr>
             </tbody>
         </table>
     </div>
